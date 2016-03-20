81e75eebacb92bd533b08bab86bb3adb769029be
Jenny Traer <lunalikesart@gmail.com>
2016-03-20 22:28:12 +0000

    Add map to farm show page

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,7 +2,7 @@ class Farm < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   geocoded_by :address
-  after_validation :geocode 
+  after_validation :geocode
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true
